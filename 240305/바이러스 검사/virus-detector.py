2ecdedd33d2197e43b26ae1fae6901bf92e67886
Choi Jeongmin <89333351+likerhythm@users.noreply.github.com>
2024-03-05 15:36:24 +0900

    Title: 바이러스 검사, Time: 527ms, Memory: 126MB, Status: Passed - CodeTree

--- 240305/바이러스 검사/virus-detector.py
+++ 240305/바이러스 검사/virus-detector.py
@@ -4,10 +4,12 @@ n = int(input())
 REST = list(map(int, input().split()))
 LDR, MBR = map(int, input().split())
 
+# print(REST)
 answer = 0
 for R in REST:
-    answer =+ 1
+    answer += 1
     if R-LDR > 0:
-        print(math.ceil((R-LDR)/MBR))
-        answer =+ math.ceil((R-LDR)/MBR)
+        # print(math.ceil((R-LDR)/MBR))
+        answer += math.ceil((R-LDR)/MBR)
+    # print("answer:", answer)
 print(answer)
