84c07cde878496e6ead5471c1f336ae70d16fa7f
Choi Jeongmin <89333351+likerhythm@users.noreply.github.com>
2024-03-05 15:26:20 +0900

    Title: 바이러스 검사, Time: 101ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240305/바이러스 검사/virus-detector.py
+++ 240305/바이러스 검사/virus-detector.py
@@ -6,5 +6,7 @@ LDR, MBR = map(int, input().split())
 
 answer = 0
 for R in REST:
-    answer = answer + 1 + math.ceil((R-LDR)/MBR)
+    answer = answer + 1
+    if R-LDR > 0:
+        answer =+ math.ceil((R-LDR)/MBR)
 print(answer)
