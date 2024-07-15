21228e3f7d59487b4d7382227a54a4c6d2678ac4
Choi Jeongmin <89333351+likerhythm@users.noreply.github.com>
2024-07-15 13:54:56 +0900

    Title: 쌓인 숫자의 순차적 이동, Time: 143ms, Memory: 25MB, Status: Passed - CodeTree

--- 240715/쌓인 숫자의 순차적 이동/sequential-movement-of-stacked-numbers.py
+++ 240715/쌓인 숫자의 순차적 이동/sequential-movement-of-stacked-numbers.py
@@ -42,18 +42,21 @@ for num in num_order:
 
     if max_num == 0:
         continue
+    
+    # print(f'num={num}')
     # index 수정
     new_index = len(arr[rx][ry])
     for i in range(len(arr[x][y][index:])):
-        num_index[i] = [rx, ry, new_index + i]
+        temp = arr[x][y][index + i]
+        num_index[temp] = [rx, ry, new_index + i]
+        # print(f'num_index[{temp}]=[{rx}, {ry}, {new_index+i}]')
 
     # 숫자들 옮기기
     arr[rx][ry].extend(arr[x][y][index:])
     arr[x][y] = arr[x][y][:index]
     
     
-    # print(f'num={num}')
-    # print(num_index)
+    # print(num_index[4])
     # print_arr(arr)
 
 for i in range(n):
