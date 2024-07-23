22b9f66d6755272b30971bf8a9a59065f6aaff9e
Choi Jeongmin <89333351+likerhythm@users.noreply.github.com>
2024-07-23 19:40:17 +0900

    Title: 피보나치 수, Time: 218ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240723/피보나치 수/fibonacci-number.py
@@ -0,0 +1,11 @@
+N = int(input())
+dp = [0] * (N + 1)
+
+dp[1] = 1
+if N == 1:
+    print(1)
+elif N >= 2:
+    dp[2] = 1
+    for i in range(3, N + 1):
+        dp[i] = dp[i - 1] + dp[i - 2]
+    print(dp[N])
