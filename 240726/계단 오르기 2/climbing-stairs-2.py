64cd5e203fd670b77e0c10e1c569f2459cf387e7
Choi Jeongmin <89333351+likerhythm@users.noreply.github.com>
2024-07-26 15:46:21 +0900

    Title: 계단 오르기 2, Time: 118ms, Memory: 24MB, Status: Passed - CodeTree

--- 240726/계단 오르기 2/climbing-stairs-2.py
+++ 240726/계단 오르기 2/climbing-stairs-2.py
@@ -1,17 +1,22 @@
-# dp[i][j]: i번째 계단을 올라왔고, 지금까지 한 칸 올라간 횟수가 j번인 경우 최대 동전 개수
+# dp[i][j]: i번째 계단을 올라왔고, 지금까지 한 칸 올라간 횟수가 j+1번인 경우 최대 동전 개수
 
 import sys
 INT_MIN = -sys.maxsize
 
 n = int(input())
-coins = list(map(int, input().split()))
+coins = [0] + list(map(int, input().split()))
 
-dp = [[INT_MIN] * 3 for _ in range(n)]
-dp[0][1] = coins[0]
-dp[1][0] = coins[1]
-dp[1][2] = coins[0] + coins[1]
-for i in range(2, n):
-    dp[i][0] = dp[i - 2][0] + coins[i]
-    dp[i][1] = max(dp[i - 2][1], dp[i - 1][0]) + coins[i]
-    dp[i][2] = max(dp[i - 2][2], dp[i - 1][1]) + coins[i]
+dp = [[INT_MIN] * 4 for _ in range(n + 1)]
+
+dp[1][1] = coins[1]
+dp[2][0] = coins[2]
+dp[2][2] = coins[1] + coins[2]
+if n > 2:
+    dp[3][1] = max(dp[1][1], dp[2][0]) + coins[3]
+    dp[3][3] = coins[1] + coins[2] + coins[3]
+    for i in range(4, n + 1):
+        dp[i][0] = dp[i - 2][0] + coins[i]
+        dp[i][1] = max(dp[i - 2][1], dp[i - 1][0]) + coins[i]
+        dp[i][2] = max(dp[i - 2][2], dp[i - 1][1]) + coins[i]
+        dp[i][3] = max(dp[i - 2][3], dp[i - 1][2]) + coins[i]
 print(max(dp[-1]))
