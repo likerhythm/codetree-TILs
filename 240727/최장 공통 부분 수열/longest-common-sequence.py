0edb95eef25e24b3f4a238b9c3e4067b916c5e9f
Choi Jeongmin <89333351+likerhythm@users.noreply.github.com>
2024-07-27 12:49:55 +0900

    Title: 최장 공통 부분 수열, Time: 146ms, Memory: 32MB, Status: Passed - CodeTree

--- 240727/최장 공통 부분 수열/longest-common-sequence.py
+++ 240727/최장 공통 부분 수열/longest-common-sequence.py
@@ -1,23 +1,24 @@
-A = input()
-B = input()
+A = ' ' + input()
+B = ' ' + input()
 len_A = len(A)
 len_B = len(B)
 
-dp = [[0] * len_B for _ in range(len_A)] # dp[i][j]: A의 i번째, B의 j번째 문자까지 고려하였을 때 최장공통부분수열
-if A[0] == B[0]:
-    dp[0][0] = 1
-for i in range(len_A):
-    if B[0] == A[i]:
-        dp[i][0] = 1
-    else:
-        dp[i][0] = dp[i - 1][0]
-    if A[0] == B[i]:
-        dp[0][i] = 1
-    else:
-        dp[0][i] = dp[0][i - 1]
-
+dp = [[0] * (len_B) for _ in range(len_A)] # dp[i][j]: A의 i번째, B의 j번째 문자까지 고려하였을 때 최장공통부분수열
+if A[1] == B[1]:
+    dp[1][1] = 1
 for i in range(1, len_A):
-    for j in range(1, len_B):
+    if B[1] == A[i]:
+        dp[i][1] = 1
+    else:
+        dp[i][1] = dp[i - 1][1]
+for i in range(1, len_B):
+    if A[1] == B[i]:
+        dp[1][i] = 1
+    else:
+        dp[1][i] = dp[1][i - 1]
+
+for i in range(2, len_A):
+    for j in range(2, len_B):
         if A[i] == B[j]:
             dp[i][j] = dp[i - 1][j - 1] + 1
         else:
