788bc6e8f81145169a1e391e16554ad4bc7edd7e
Choi Jeongmin <89333351+likerhythm@users.noreply.github.com>
2024-09-11 14:04:37 +0900

    Title: 나무박멸, Time: 454ms, Memory: 29MB, Status: Wrong Answer - CodeTree

---
+++ 240911/나무박멸/tree-kill-all.py
@@ -0,0 +1,128 @@
+n, m, k, c = map(int, input().split())
+graph = [list(map(int, input().split())) for _ in range(n)]
+walls = set()
+for i in range(n):
+    for j in range(n):
+        if graph[i][j] == -1:
+            walls.add((i, j))
+
+KILL = -c - 1
+WALL = -1
+
+dxs = [-1, 0, 1, 0]
+dys = [0, 1, 0, -1]
+
+def in_range(x, y):
+    return 0 <= x < n and 0 <= y < n
+
+def grow(): # 나무 성장
+    for x in range(n):
+        for y in range(n):
+            if graph[x][y] > 0:
+                cnt = 0
+                for dx, dy in zip(dxs, dys):
+                    nx = x + dx
+                    ny = y + dy
+                    if in_range(nx, ny) and graph[nx][ny] > 0:
+                        cnt += 1
+                graph[x][y] += cnt
+
+
+def breed(): # 나무 번식
+    temp_graph = [[0] * n for _ in range(n)]
+    for i in range(n):
+        for j in range(n):
+            temp_graph[i][j] = graph[i][j]
+    for x in range(n):
+        for y in range(n):
+            if graph[x][y] > 0:
+                cnt = 0
+                target = []
+                for dx, dy in zip(dxs, dys):
+                    nx = x + dx
+                    ny = y + dy
+                    if in_range(nx, ny) and graph[nx][ny] == 0:
+                        target.append([nx, ny])
+                        cnt += 1
+                if cnt > 0:
+                    value = graph[x][y] // cnt
+                    for nx, ny in target:
+                        temp_graph[nx][ny] += value
+    for i in range(n):
+        for j in range(n):
+            graph[i][j] = temp_graph[i][j]
+
+
+kill_x = [-1, -1, 1, 1]
+kill_y = [-1, 1, 1, -1]
+
+def count_kill(x, y):
+    cnt = graph[x][y]
+    ignore = set()
+    for i in range(1, k + 1):
+        for j in range(4):
+            if j in ignore:
+                continue
+            dx, dy = kill_x[j], kill_y[j]
+            nx = x + dx * i
+            ny = y + dy * i
+            if in_range(nx, ny) and graph[nx][ny] > 0:
+                cnt += graph[nx][ny]
+            else:
+                ignore.add(j)
+    # print(f'kill {x},{y} = {cnt}')
+    return cnt
+
+
+
+def kill(): # 제초제
+    max_cnt = 0
+    result_x = 0
+    result_y = 0
+    for x in range(n):
+        for y in range(n):
+            if graph[x][y] > 0:
+                cnt = count_kill(x, y)
+                if max_cnt < cnt:
+                    max_cnt = cnt
+                    result_x, result_y = x, y
+    # result에 제초제 뿌리기
+    # print(f'kill {result_x}, {result_y}')
+    ignore = set()
+    graph[result_x][result_y] = KILL
+    for i in range(4):
+        dx, dy = kill_x[i], kill_y[i]
+        nx = result_x + dx
+        ny = result_y + dy
+        if in_range(nx, ny):
+            if graph[nx][ny] == 0 or graph[nx][ny] == WALL: # 벽이거나 빈 공간인 경우
+                graph[nx][ny] = KILL
+                ignore.add(i)
+            else:
+                graph[nx][ny] = KILL
+    return max_cnt
+
+def print_graph():
+    for i in range(n):
+        print(*graph[i])
+    print()
+
+answer = 0
+# print_graph()
+for _ in range(m):
+    for i in range(n):
+        for j in range(n):
+            if graph[i][j] < 0 and (i, j) not in walls:
+                graph[i][j] += 1
+    
+    # print('grow')
+    grow()
+    # print_graph()
+    # print('breed')
+    breed()
+    # print_graph()
+    # print('kill')
+    answer += kill()
+    # print_graph()
+
+print(answer)
